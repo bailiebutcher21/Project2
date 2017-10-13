d427390a1a28fb830db37f549d09ab10dec1fc12
bailiebutcher <bailiebutcher@gmail.com>
2017-10-13 15:42:12 -0600

    Added Logo to all HTML pages

--- html files/index.js
+++ html files/index.js
@@ -1,25 +1,8 @@
-var opentime;
-switch (new Date().getDay()) {
-    case 0:
-        day = "Opens at: 11:00 AM - 1:00 AM";
-        break;
-    case 1:
-        day = "Opens at: 6:00 AM - 1:00 AM";
-        break;
-    case 2:
-        day = "Opens at: 6:00 AM - 1:00 AM";
-        break;
-    case 3:
-        day = "Opens at: 6:00 AM - 1:00 AM";
-        break;
-    case 4:
-        day = "TOpens at: 6:00 AM - 1:00 AM";
-        break;
-    case 5:
-        day = "Opens at: 6:00 AM - 1:00 AM";
-        break;
-    case 6:
-        day = "Opens at: 6:00 AM - 1:00 AM";
-        break;
+function myMap() {
+    var mapProp= {
+        center:new google.maps.LatLng(51.508742,-0.120850),
+        zoom:5,
+    };
+    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
-document.getElementById("opentime").innerHTML = "Today is " + day;
+
